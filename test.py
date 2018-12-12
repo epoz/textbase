1dc5e9fef643366f5a64ec7756bbd1af6cbb31a5
George Hickman <george@ghickman.co.uk>
2018-12-12 12:27:54 +0000

    Wrap existing test in a function so pytest can find it

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import textbase
+
 example = '''# Here is a comment
 NAME John Doe
 DOB 19720601
@@ -16,19 +18,30 @@ COLOURS black
 $
 # ANd this is effectively an empty record.
 $
-'''
+"""
 
-output = [ {'NAME':['John Doe'],
-            'DOB':['19720601'], 'COLOURS':['red','green','mauve'],
-            'STORY': ['''Once upon a time there lived a frog in a far away land. He was a very sprightly frog and could jump around all day long.''']},
-           {'NAME':['Peter Piper'],
-            'DOB':['19840101'], 'COLOURS':['black','purple','white'] }
-         ]
-import textbase
 
-if __name__ == '__main__':
-    t = textbase.TextBase(example)
-    tmp = []
-    for x in t:
-        tmp.append(x)
-    assert tmp == output
+def test_main():
+    expected = [
+        {
+            "NAME": ["John Doe"],
+            "DOB": ["19720601"],
+            "COLOURS": ["red", "green", "mauve"],
+            "STORY": [
+                """Once upon a time there lived a frog in a far away land. He was a very sprightly frog and could jump around all day long."""
+            ],
+        },
+        {
+            "NAME": ["Peter Piper"],
+            "DOB": ["19840101"],
+            "COLOURS": ["black", "purple", "white"],
+        },
+    ]
+
+    output = [x for x in textbase.TextBase(example)]
+
+    assert output == expected
+
+
+if __name__ == "__main__":
+    test_main()
